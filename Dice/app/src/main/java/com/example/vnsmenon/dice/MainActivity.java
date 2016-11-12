85e5efd165dd852690cbebdbb6c582f2c30228d3
Sooraj Menon <vn.soorajmenon@gmail.com>
2016-11-12 20:06:26 +0530

    Few more changes
    A few changes and error corrections

--- Dice/app/src/main/java/com/example/vnsmenon/dice/MainActivity.java
+++ Dice/app/src/main/java/com/example/vnsmenon/dice/MainActivity.java
@@ -15,9 +15,7 @@ public class MainActivity extends AppCompatActivity {
         private int comp_score;
         private int comp_buff_score;
 
-       public Button Roll = (Button)findViewById(R.id.Roll);
-       public Button Hold = (Button) findViewById(R.id.Hold);
-       public Button Reset = (Button) findViewById(R.id.Reset);
+        public Button Roll, Hold, Reset;
 
         Random rand = new Random();
 
@@ -25,108 +23,128 @@ public class MainActivity extends AppCompatActivity {
 
     int i;
 
-    final TextView CompScorer = (TextView) findViewById(R.id.CompScore);
+    TextView CompScorer, UserScorer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Roll = (Button) findViewById(R.id.Roll);
+        Hold = (Button) findViewById(R.id.Hold);
+        Reset = (Button) findViewById(R.id.Reset);
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_score = 0;
+                comp_score = 0;
+                comp_buff_score = 0;
+                user_score_buff = 0;
+                UserScorer.setText("0");
+                CompScorer.setText("0");
+                Roll.setEnabled(true);
+                Hold.setEnabled(true);
+            }
+        });
 
-        final TextView UserScorer = (TextView) findViewById(R.id.UserScore);
+        CompScorer = (TextView) findViewById(R.id.CompScore);
 
+        UserScorer = (TextView) findViewById(R.id.UserScore);
+        user_score = 0;
+        comp_score = 0;
 
         UserScorer.setText("0");
         CompScorer.setText("0");
 
 
-        final ImageView diceImage = (ImageView) findViewById(R.id.Dice);
-        //Button Roll = (Button) findViewById(R.id.Roll);
-
-        // calling onClick() method
+    }
 
 
-        // Hold.setOnClickListener(this);
 
-        //Reset.setOnClickListener(this);
 
-        Roll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                i = rand.nextInt(diceArray.length);
+     public void user(){
 
-                diceImage.setImageResource(diceArray[i]);
-                switch (i) {
-                    case 0:
-                        user_score += user_score_buff;
-                        UserScorer.setText(user_score);
-                        user_score_buff = 0;
-                        computerScore();
+         Roll.setEnabled(true);
+         Hold.setEnabled(true);
 
-                        break;
-                    case 1:
-                        user_score_buff += 2;
-                        user_score = user_score_buff;
-                        break;
-                    case 2:
-                        user_score_buff += 3;
-                        user_score = user_score_buff;
-                        break;
-                    case 3:
-                        user_score_buff += 4;
-                        user_score = user_score_buff;
-                        break;
-                    case 4:
-                        user_score_buff += 5;
-                        user_score = user_score_buff;
-                        break;
-                    case 5:
-                        user_score_buff += 6;
-                        user_score = user_score_buff;
-                        break;
+
+            Roll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final ImageView diceImage = (ImageView) findViewById(R.id.Dice);
+
+                    i = rand.nextInt(diceArray.length);
+                    user_score = Integer.parseInt(UserScorer.getText().toString());
+                    diceImage.setImageResource(diceArray[i]);
+
+                    switch (i) {
+                        case 0:
+                            user_score += user_score_buff;
+                            UserScorer.setText(Integer.toString(user_score));
+                            user_score_buff = 0;
+                            computerScore();
+
+                            break;
+                        case 1:
+                            user_score_buff += 2;
+                            user_score = user_score_buff;
+                            break;
+                        case 2:
+                            user_score_buff += 3;
+                            user_score = user_score_buff;
+                            break;
+                        case 3:
+                            user_score_buff += 4;
+                            user_score = user_score_buff;
+                            break;
+                        case 4:
+                            user_score_buff += 5;
+                            user_score = user_score_buff;
+                            break;
+                        case 5:
+                            user_score_buff += 6;
+                            user_score = user_score_buff;
+                            break;
+                    }
+
                 }
 
-            }
-        });
+            });
 
 
-        Hold.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserScorer.setText(user_score);
-                user_score_buff = 0;
-            }
-        });
+            Hold.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    UserScorer.setText(Integer.toString(user_score));
+                    user_score_buff = 0;
+                }
+            });
 
 
-        Reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserScorer.setText("0");
-                CompScorer.setText("0");
-            }
-        });
-    }
+
+        }
 
     public void computerScore(){
 
         i= rand.nextInt(diceArray.length);
-        Button Roll = (Button)findViewById(R.id.Roll);
-        Button Hold = (Button)findViewById(R.id.Hold);
+
         Roll.setEnabled(false);
         Hold.setEnabled(false);
 
-        while(i>0){
+        while(i>=0){
             switch (i) {
-                case 0: CompScorer.setText(comp_score);
+                case 0: CompScorer.setText(Integer.toString(comp_score));
                         comp_buff_score = 0;
-                        break;
+                        user();
+
+
                 case 1:
                     comp_buff_score += 2;
                     comp_score = comp_buff_score;
                     break;
                 case 2:
+
                     comp_buff_score += 3;
                     comp_score = comp_buff_score;
                     break;
@@ -143,9 +161,14 @@ public class MainActivity extends AppCompatActivity {
                     comp_score = comp_buff_score;
                     break;
             }
-            CompScorer.setText(comp_score);
+            CompScorer.setText(Integer.toString(comp_score));
+
+
         }
+
+
     }
+
 
 }
 
