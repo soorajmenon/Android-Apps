2f0bf987431f3b7f271c401bad99e10f65fd63d9
Sooraj Menon <vn.soorajmenon@gmail.com>
2017-01-10 21:21:54 +0530

    Splash Screen finalsied

--- BurnApp/app/src/main/java/com/example/vnsmenon/burnapp/MainActivity.java
+++ BurnApp/app/src/main/java/com/example/vnsmenon/burnapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.vnsmenon.burnapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int splash_timeout = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+            }
+        }, splash_timeout);
+
+
 
     }
 }
