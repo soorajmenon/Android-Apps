43014c447df4c94bf0756cffba0f0ba66d01d2ba
Sooraj Menon <vn.soorajmenon@gmail.com>
2017-01-11 22:19:14 +0530

    New layout

---
+++ BurnApp/app/src/main/java/com/example/vnsmenon/burnapp/Food.java
@@ -0,0 +1,13 @@
+package com.example.vnsmenon.burnapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+    }
+}
